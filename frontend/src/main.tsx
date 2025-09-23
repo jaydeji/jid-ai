099b74abd5ebb4a38f92e2f83aba531d81ac82bd
Jide Adedeji <jideadedejifirst@gmail.com>
2025-09-23 15:32:08 -0400

    remove persist

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,38 +3,24 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 
 import './styles.css'
-import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
-import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
+import { QueryClientProvider } from '@tanstack/react-query'
 import reportWebVitals from './reportWebVitals.ts'
 
 import { ChatProvider } from './components/chat-context.tsx'
 import { queryClient } from '@/services/react-query/hooks.ts'
 import { router } from '@/routes/index.tsx'
 
-const persister = createAsyncStoragePersister({
-  storage: window.localStorage,
-})
-
 const rootElement = document.getElementById('app')
 if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <PersistQueryClientProvider
-        client={queryClient}
-        persistOptions={{ persister }}
-        onSuccess={() => {
-          // resume mutations after initial restore from localStorage was successful
-          queryClient.resumePausedMutations().then(() => {
-            queryClient.invalidateQueries()
-          })
-        }}
-      >
+      <QueryClientProvider client={queryClient}>
         <ChatProvider>
           <RouterProvider router={router} />
         </ChatProvider>
         ;
-      </PersistQueryClientProvider>
+      </QueryClientProvider>
     </StrictMode>,
   )
 }
